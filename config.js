e24f0f6056ef60972cdb3abbec37495294e24098
samith-dilshan <my.koyeb.ac@gmail.com>
2025-03-21 11:21:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=n6QmkZxC#Nd6_SR_bISVjhMbXEd4ZYwW9qYgiGwlxKIevKeNYGNw' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=OyIzwJJL#E9kulJ-N-gdHk0P3kQULPVFKC_enGqQOu4uBRfGiXYQ' : process.env.SESSION_ID,
 };
